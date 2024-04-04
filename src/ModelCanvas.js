032ca56f8e115f69d75f088d96be9c737f0cd599
loki-engr <loki98957@gmail.com>
2024-04-04 16:34:47 -0300

    fix camera on mobile, become brighter

--- src/ModelCanvas.js
+++ src/ModelCanvas.js
@@ -1,5 +1,4 @@
-// ModelCanvas.js
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { OBJLoader } from "three/addons/loaders/OBJLoader";
 import { MTLLoader } from "three/addons/loaders/MTLLoader";
 import { TGALoader } from "three/addons/loaders/TGALoader";
@@ -8,7 +7,11 @@ import { OrbitControls } from "@react-three/drei";
 import * as THREE from "three";
 
 function MyModel() {
-  const { scene } = useThree();
+  const { scene, camera } = useThree();
+  const [isMobile, setIsMobile] = useState(
+    window.innerWidth < 768 ? true : false
+  );
+
   useEffect(() => {
     const manager = new THREE.LoadingManager();
     manager.addHandler(/.tga$/i, new TGALoader());
@@ -22,34 +25,47 @@ function MyModel() {
       objLoader.setMaterials(materials);
 
       objLoader.load("cicada.obj", (object) => {
-        // object.traverse((child) => {
-        //   if (child.isMesh) {
-        //     // Use MeshBasicMaterial to display color without lighting
-        //     child.material = new THREE.MeshBasicMaterial({
-        //       map: materials.materials[child.material.name].map,
-        //       color: materials.materials[child.material.name].color,
-        //     });
-        //   }
-        // });
         object.traverse((child) => {
           if (child.isMesh) {
-            // Apply MeshStandardMaterial for realistic lighting
             const texture = materials.materials[child.material.name].map;
             child.material = new THREE.MeshStandardMaterial({
               map: texture,
             });
           }
         });
-        object.scale.multiplyScalar(0.1);
+
+        // Set the model size relative to the viewport width
+        if (isMobile) object.scale.set(0.04, 0.06, 0.06);
+        else object.scale.set(0.06, 0.06, 0.06);
+
+        // Center the model
+        const box = new THREE.Box3().setFromObject(object);
+        const center = box.getCenter(new THREE.Vector3());
+        object.position.sub(center);
+
         scene.add(object);
       });
     });
-    const ambientLight = new THREE.AmbientLight(0x404040);
-    const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
+
+    // Brighter lights
+    const ambientLight = new THREE.AmbientLight(0x404040, 20); // Increased intensity
+    const directionalLight = new THREE.DirectionalLight(0xffffff, 8); // Increased intensity
     directionalLight.position.set(0, 1, 0);
     scene.add(ambientLight);
     scene.add(directionalLight);
-  }, [scene]);
+
+    // Handle window resize for device scale
+    function handleResize() {
+      setIsMobile(window.innerWidth < 768 ? true : false);
+    }
+
+    window.addEventListener("resize", handleResize);
+
+    // Clean up
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, [scene, camera]);
 
   return null;
 }
